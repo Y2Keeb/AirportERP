ef1604113b24b90379c72a468cd59f216ee04948
LindseyJorissen <Lindsey.jorissen@gmail.com>
2025-03-18 21:31:52 +0100

    create tables SQL

---
+++ src/SQL/create_tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL CHECK (role IN ('user', 'admin', 'staff')),
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Employees (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    job_team VARCHAR(255) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL
+);
+CREATE TABLE Flights (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    airline VARCHAR(255) NOT NULL,
+    departure DATETIME NOT NULL,
+    arrival DATETIME NOT NULL,
+	status VARCHAR(50) NOT NULL,
+    gate VARCHAR(50) NOT NULL,
+    plane_type VARCHAR(50) NOT NULL,
+    total_seats INTEGER NOT NULL CHECK (total_seats > 0),
+    seats_taken INTEGER NOT NULL DEFAULT 0
+);
+CREATE TABLE Bookings (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    flight_id INTEGER NOT NULL,
+    booking_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+	status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
+    FOREIGN KEY (flight_id) REFERENCES Flights(id) ON DELETE CASCADE
+);
