667698efbae6cece8f021d23c3e18e6c3e927b21
LindseyJorissen <Lindsey.jorissen@gmail.com>
2025-03-19 09:30:51 +0100

    initial commit new branch

---
+++ ticket_system.py
@@ -0,0 +1,8 @@
+#1 search for flights based on filters
+#2 user selects flight from available flights
+#3 user can add additional packages which should be added to price and database
+#4 user confirms - dummy payment screen
+
+#in background program will remove 1 available seat after user confirms from flight
+
+
