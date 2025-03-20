9e8bc08ff14ec3e1581fe2b64a3d333aec148a02
LindseyJorissen <Lindsey.jorissen@gmail.com>
2025-03-20 21:56:24 +0100

    added themes folder with customtkinter themes

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 """
-The mainscreen
+The mainscreen using CustomTkinter
 """
-import tkinter as tk
+import customtkinter
 from login_screen import LoginScreen
 
-root = tk.Tk()
-app = LoginScreen(root)
-root.bind("<Return>", lambda event: app.login())
-root.mainloop()
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("themes/rime.json")
 
+root = customtkinter.CTk()
+root.title("Airport ERP Login")
+
+app = LoginScreen(root)
+
+root.bind("<Return>", lambda event: app.login())
+
+root.mainloop()
