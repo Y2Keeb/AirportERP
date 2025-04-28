229fe85834aef66799b01826d5717625ce188500
LindseyJorissen <Lindsey.jorissen@gmail.com>
2025-04-28 21:51:32 +0200

    easter egg bjorn

--- views/bjorn_easter_egg.py
+++ views/bjorn_easter_egg.py
@@ -4,12 +4,18 @@ class BjornEasterEgg(ctk.CTkToplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.title("BJORN MODE ACTIVATED")
-        self.geometry("400x300")
+        self.geometry("800x600")
+
+        self.lift()
+        self.attributes('-topmost', True)
+        self.after(100, lambda: self.attributes('-topmost', False))
+        self.focus_force()
+
         self.label = ctk.CTkLabel(self, text="✨ You found the BJORN Easter Egg! ✨", font=("Comic Sans MS", 20, "bold"))
         self.label.pack(pady=30)
 
-        self.bear_msg = ctk.CTkLabel(self, text="Because wine not", font=("Arial", 14))
-        self.bear_msg.pack(pady=10)
+        self.lbl_temp= ctk.CTkLabel(self, text="Because wine not", font=("Arial", 14))
+        self.lbl_temp.pack(pady=10)
 
         self.close_button = ctk.CTkButton(self, text="Close", command=self.destroy)
         self.close_button.pack(pady=20)
