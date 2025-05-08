6bac6a41741de1b96fee6873575d9cf61bee25f7
LindseyJorissen <Lindsey.jorissen@gmail.com>
2025-05-08 20:24:34 +0200

    added airline screen entry fields

--- views/airline_screen.py
+++ views/airline_screen.py
@@ -13,25 +13,55 @@ class AirlineScreen(BaseWindow):
         }
         self.frame_main = ctk.CTkFrame(self.root)
         self.frame_main.pack(fill="both", expand=True, padx=10, pady=10)
+        self.frame_content = ctk.CTkFrame(
+            self.frame_main,
+            border_color="black",
+            border_width=3
+        )
+        self.frame_content.place(relx=0.5, rely=0.525, anchor="center", relwidth=0.4, relheight=0.75)
         set_theme()
 
-        self.lbl_airline = ctk.CTkLabel(self.frame_main, text="Airline: ")
-        self.airline_entry = ctk.CTkEntry(self.frame_main, width=150)
-        self.airline_entry.insert(0, self.full_name)
-        self.lbl_from_location = ctk.CTkLabel(self.frame_main, text="From Location: ")
-        self.lbl_to_location = ctk.CTkLabel(self.frame_main, text="To Location: ")
+        self.lbl_airline = ctk.CTkLabel(self.frame_content, text="Airline: ")
+        self.lbl_from_location = ctk.CTkLabel(self.frame_content, text="From Location: ")
+        self.lbl_to_location = ctk.CTkLabel(self.frame_content, text="To Location: ")
+        self.lbl_departure_date = ctk.CTkLabel(self.frame_content, text="Departure date: ")
+        self.lbl_arrival_date = ctk.CTkLabel(self.frame_content, text="Arrival date: ")
+        self.lbl_plane_type = ctk.CTkLabel(self.frame_content, text="Plane Type: ")
+        self.lbl_total_seats = ctk.CTkLabel(self.frame_content, text="Total Seats: ")
+        self.lbl_price = ctk.CTkLabel(self.frame_content, text="Price: ")
 
+        self.airline_entry = ctk.CTkEntry(self.frame_content, width=150)
+        self.airline_entry.insert(0, self.full_name)
+        self.from_location_entry = ctk.CTkEntry(self.frame_content,width=150)
+        self.to_location_entry = ctk.CTkEntry(self.frame_content,width=150)
+        self.departure_date_entry = ctk.CTkEntry(self.frame_content,width=150)
+        self.arrival_date_entry = ctk.CTkEntry(self.frame_content,width=150)
+        self.plane_type_entry = ctk.CTkEntry(self.frame_content,width=150)
+        self.total_seat_entry = ctk.CTkEntry(self.frame_content,width=150)
+        self.price_entry = ctk.CTkEntry(self.frame_content,width=150)
         self.create_widgets()
 
     def create_widgets(self):
-        greeting_label = ctk.CTkLabel(self.frame_main, text=f"Welcome {self.full_name}!\nAdd New Flight Information!", font=("Arial", 20))
-        greeting_label.grid(row=0, column=0, pady=10, padx=10, sticky="w")
-
-        self.lbl_airline.grid(row=1, column=0,padx=10, pady=10,sticky="w")
-        self.airline_entry.grid(row=1, column=1, padx=10,pady=10,sticky="w")
-        self.lbl_from_location.grid(row=2,column=0,padx=10,pady=10,sticky="w")
-        self.lbl_to_location.grid(row=3,column=0,padx=10,pady=10,sticky="w")
-
+        greeting_label = ctk.CTkLabel(self.frame_main, text=f"Welcome {self.full_name}!\nAdd Available Flight Information Below.", font=("Arial", 20))
+        greeting_label.grid(row=0, column=0, pady=10, padx=5, sticky="ew")
+        #labels
+        self.lbl_airline.grid(row=1, column=0,padx=10, pady=8,sticky="w")
+        self.lbl_from_location.grid(row=2,column=0,padx=10,pady=8,sticky="w")
+        self.lbl_to_location.grid(row=3,column=0,padx=10,pady=8,sticky="w")
+        self.lbl_departure_date.grid(row=4,column=0,padx=10,pady=8,sticky="w")
+        self.lbl_arrival_date.grid(row=5,column=0,padx=10,pady=8,sticky="w")
+        self.lbl_plane_type.grid(row=6,column=0,padx=10,pady=8,sticky="w")
+        self.lbl_total_seats.grid(row=7,column=0,padx=10,pady=8,sticky="w")
+        self.lbl_price.grid(row=8,column=0,padx=10,pady=8,sticky="w")
+        #entries
+        self.airline_entry.grid(row=1, column=1, padx=10,pady=8,sticky="w")
+        self.from_location_entry.grid(row=2, column=1, padx=10,pady=8,sticky="w")
+        self.to_location_entry.grid(row=3, column=1, padx=10,pady=8,sticky="w")
+        self.departure_date_entry.grid(row=4, column=1, padx=10,pady=8,sticky="w")
+        self.arrival_date_entry.grid(row=5, column=1, padx=10,pady=8,sticky="w")
+        self.plane_type_entry.grid(row=6, column=1, padx=10,pady=8,sticky="w")
+        self.total_seat_entry.grid(row=7, column=1, padx=10,pady=8,sticky="w")
+        self.price_entry.grid(row=8, column=1, padx=10,pady=8,sticky="w")
 
     def get_airline_name(self):
         cursor = mydb.cursor()
@@ -52,13 +82,3 @@ class AirlineScreen(BaseWindow):
         finally:
             if cursor:
                 cursor.close()
-"""
-'from_location'
-'to_location'
-'departure date'
-'arrival date'
-'plane_type'
-'total_seats'
-'price'
-'flight_id'
-"""
